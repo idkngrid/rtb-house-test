d84c79fb4b7a55a8bcf8d02a9a2f1e6cfe3d880e
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-24 23:38:43 -0300

    Add home and info content and styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,30 @@
-import { Navbar } from "../../components/Navbar";
+import { Card } from "../../components/Card";
+import { Header } from "../../components/Header";
 
 export function Home() {
     return (
         <div>
-            <Navbar />
+            <Header />
+            
+            <section className="section">
+                <div className="container">
+                    <h2 className="section__title">Orders</h2>
+                    <div className="grid">
+                        <Card 
+                            seller="1"
+                            totalCount="9999.99"
+                        />
+                        <Card 
+                            seller="2"
+                            totalCount="9999.99"
+                        />
+                        <Card 
+                            seller="3"
+                            totalCount="9999.99"
+                        />
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
--- src/pages/Info/index.jsx
+++ src/pages/Info/index.jsx
@@ -1,3 +1,23 @@
+import { Header } from "../../components/Header"
+
 export function Info() {
-    return <div>Infoooo</div>
+    return (
+        <div>
+            <Header />
+            
+            <section className="section">
+                <div className="container">
+                    <h2 className="section__title">Info</h2>
+                    <p>Developer name</p>
+                    <p>This mini-project was created by using:</p>
+                    <ul>
+                        <li>Technology A</li>
+                        <li>Technology B</li>
+                        <li>Technology C</li>
+                        <li>Technology D</li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+    )
 }
