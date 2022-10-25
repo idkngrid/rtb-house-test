c621dbdcdf92cbba347c2294655b8b6c7f3dfca2
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-25 12:27:51 -0300

    Add header and footer component to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 import { Router } from './Router';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Router />
+      <Footer />
     </BrowserRouter>
   )
 }
