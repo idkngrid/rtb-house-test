cf92795bbaa1195a361976caeb7b1150a5639774
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-25 22:21:17 -0300

    Get card data from json

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,7 +3,7 @@ import styles from './Card.module.css';
 export function Card({ seller, totalCount }) {
     return (
         <div className={styles.card}>
-            <h2 className={styles.card__title}>Total of Seller #{seller}</h2>
+            <h2 className={styles.card__title}>Total of {seller}</h2>
             <span className={styles.card__count}>${totalCount}</span>
         </div>
     )
