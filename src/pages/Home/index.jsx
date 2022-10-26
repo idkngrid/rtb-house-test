7c2479fdccdcb5d3c057d3a528b7617a589ba338
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-26 13:21:58 -0300

    Add sellers card to home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,27 +6,31 @@ import { Table } from "../../components/Table";
 import Orders from "../../data/orders.json";
 import Sellers from "../../data/sellers.json";
 
-
 export function Home() {
     const [orders, setOrders] = useState(Orders);
     const [sellers, setSellers] = useState(Sellers);
+    const [totalCount, setTotalCount] = useState(0);
     const [searchInput, setSearchInput] = useState('');
 
-    function searchOrders(searchValue) {
+    useEffect(() => {
+        const ordersSellers = orders.filter(order => order.seller === sellers[0].id).map(value => value.price).reduce((acc, amount) => acc + amount);
+        setTotalCount(ordersSellers);
+    }, [orders])
+
+
+    function handleSearchOrders(searchValue) {
         setSearchInput(searchValue);
         
         if(searchInput !== '') {
             const filteredData = orders.filter((order) => {
                 return Object.values(order).join('').toLowerCase().includes(searchInput.toLowerCase())
             })
-                setOrders(filteredData);
-            }
-            else {
-                setOrders(orders);
-            }
+            setOrders(filteredData);
+        }
+        else {
+            setOrders(orders);
+        }
     }
-
-    console.log(orders);
 
     return (
         <div>
@@ -38,17 +42,16 @@ export function Home() {
                             <Card 
                                 key={seller.id}
                                 seller={seller.name}
-                                totalCount="9999.99"
+                                totalCount={totalCount}
                             />
                         ))}
                     </div>
                 </div>
             </section>
 
-            {/* Search */}
             <section className="section--py">
                 <div className="container">
-                    <Search onChange={(e) => searchOrders(e.target.value)}/>
+                    <Search onChange={(e) => handleSearchOrders(e.target.value)}/>
                 </div>
             </section>
 
