cbae53300e2c8d1a82c2a67c59b0326b587056dd
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-27 11:49:41 -0300

    Add some info content

--- src/pages/Info/index.jsx
+++ src/pages/Info/index.jsx
@@ -4,13 +4,15 @@ export function Info() {
             <section className="section--pall">
                 <div className="container">
                     <h2 className="section__title">Info</h2>
-                    <p>Developer name</p>
-                    <p>This mini-project was created by using:</p>
+                    <p>Ingrid de Camargo Pereira</p>
+                    <p>For this project, I tried using the least libraries as possible to really challenge myself on doing things from scratch.</p>
+                    <p>Technologies used:</p>
                     <ul>
-                        <li>Technology A</li>
-                        <li>Technology B</li>
-                        <li>Technology C</li>
-                        <li>Technology D</li>
+                        <li>React</li>
+                        <li>Node.js</li>
+                        <li>CSS Modules</li>
+                        <li>Phosphor React</li>
+                        <li>Github</li>
                     </ul>
                 </div>
             </section>
