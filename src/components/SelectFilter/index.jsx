bee96d0a0884798719530d6d40d48b838bcd9bd7
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-27 15:34:09 -0300

    Add select filter component

---
+++ src/components/SelectFilter/index.jsx
@@ -0,0 +1,11 @@
+export function SelectFilter({ fields }) {
+    return (
+        <select name="" id="">
+            <option value="">teste</option>
+            <option value="">teste</option>
+            <option value="">teste</option>
+            <option value="">teste</option>
+            <option value="">teste</option>
+        </select>
+    )
+}
