694e167a53c18770abff3927cc5571990fb36fcd
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-27 21:52:16 -0300

    Add select filter functionality

--- src/components/SelectFilter/index.jsx
+++ src/components/SelectFilter/index.jsx
@@ -1,11 +1,26 @@
-export function SelectFilter({ fields }) {
+import Orders from '../../data/orders.json';
+import Sellers from '../../data/sellers.json';
+
+import styles from './SelectFilter.module.css';
+
+export function SelectFilter({ filterValueSelected }) {
+
+    const countries = [
+        { id: 1, country: "BRA" },
+        { id: 2, country: "ARG" },
+        { id: 3, country: "MEX" },
+    ]
+
+    function onHandleFilterSelect(e) {
+        filterValueSelected(e.target.value);
+    }
+
     return (
-        <select name="" id="">
-            <option value="">teste</option>
-            <option value="">teste</option>
-            <option value="">teste</option>
-            <option value="">teste</option>
-            <option value="">teste</option>
+        <select name="" id="" className={styles.select} onChange={onHandleFilterSelect}>
+            <option value="all">All</option>
+            {countries.map(country => (
+                <option className={styles.option} value={country.country} key={country.id}>{country.country}</option>
+            ))}
         </select>
     )
 }
