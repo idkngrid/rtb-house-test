6e2c782124d91e9087f49c5c323097e462ba6348
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-27 21:53:07 -0300

    Add select filter functionality

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,17 +10,43 @@ import { SelectFilter } from "../../components/SelectFilter";
 
 export function Home() {
     const [filteredOrders, setFilteredOrders] = useState(Orders);
-    const [sellers] = useState(Sellers);
+    const [filterSelectedValue, setFilterSelectedValue] = useState('all');
     const [totalCount, setTotalCount] = useState(0);
 
     const [currentPage, setCurrentPage] = useState(1);
     const [ordersPerPage] = useState(10);
 
+    const ordersSellers = filteredOrders.reduce((total, order) => {
+        const { seller, price } = order;
+        if(seller) {
+            if(total[seller]) {
+                total[seller] += price;
+            } else {
+                total[seller] = price;
+            }
+        }
+        return total;
+    }, {})
+
     useEffect(() => {
-        
-            const ordersSellers = filteredOrders.filter(order => order.seller === sellers[0].id).map(value => value.price).reduce((acc, amount) => acc + amount);
-            setTotalCount(ordersSellers);
+        const entries = Object.entries(ordersSellers);
+        entries.map(entry => {
+            if(entry[0] == 1) {
+                setTotalCount(entry[1]);
+            } else if(entry[0] == 2) {
+                setTotalCount(entry[1]);
+            } else {
+                setTotalCount(entry[1]);
+            }
+        });
     }, [])
+
+        // const ordersSellers = 
+        //     filteredOrders.filter(order => order.seller === )
+        //     .map(value => value.price)
+        //     .reduce((acc, amount) => acc + amount);
+
+        // setTotalCount(ordersSellers);
 
     // Setting variables for the pagination
     const indexLastOrder = currentPage * ordersPerPage;
@@ -44,6 +70,22 @@ export function Home() {
         }
     }
 
+    function onFilterSelected(selectedValue) {
+        setFilterSelectedValue(selectedValue);
+    }
+
+    const filterSelected = Orders.filter((order) => {
+        if(filterSelectedValue === "ARG") {
+            return order.country === "ARG";
+        } else if(filterSelectedValue === "BRA") {
+            return order.country === "BRA";
+        } else if(filterSelectedValue === "MEX") {
+            return order.country === "MEX";
+        } else {
+            return order;
+        }
+    })
+
 return (
     <div>
         <section className="section--pall">
@@ -51,7 +93,7 @@ return (
 
             <h2 className="section__title">Orders</h2>
                     <div className="grid">
-                        {sellers.map((seller) => (
+                        {Sellers.map((seller) => (
                             <Card 
                                 key={seller.id}
                                 seller={seller.name}
@@ -65,17 +107,17 @@ return (
             <section className="section--py">
                 <div className="container grid">
                     <Search onChange={(e) => handleSearchOrders(e.target.value)}/>
-                    <SelectFilter />
-                    <SelectFilter />
+                    
+                    <SelectFilter filterValueSelected={onFilterSelected} />
                 </div>
             </section>
 
             <section className="section--pall">
                 <div className="container">
-                    <Table data={currentOrders} />  
+                    <Table data={filterSelected} />  
                     <Pagination 
                         ordersPerPage={ordersPerPage}
-                        totalOrders={filteredOrders.length}
+                        totalOrders={Orders.length}
                         onHandlePagination={handlePagination}
                     />
                 </div>
