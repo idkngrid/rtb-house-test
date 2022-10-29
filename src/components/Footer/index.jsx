b3ac5b565e27c3819018e1a1125ac90c1e6ed712
Ingrid Camargo <ingrid.camargopr@gmail.com>
2022-10-29 16:51:07 -0300

    Fix some styles

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 export function Footer() {
     return (
         <footer className={styles.footer}>
-            <a href="#" className={styles.footer__logo}><img src="./logo.svg" alt="" /></a>
+            <a title='Back to main page' href="#" className={styles.footer__logo}><img src="./logo.svg" alt="Logo RTB House" /></a>
         </footer>
     )
 }
